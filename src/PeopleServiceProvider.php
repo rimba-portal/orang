ceebfee5cb09e6f90cc95d74f21c630b7ef589db
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-22 15:33:20 +0800

    Automated bulk repository update

---
+++ src/PeopleServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rimba\People;
+
+use Rimba\Base\BitesServiceProvider;
+
+
+class PeopleServiceProvider extends BitesServiceProvider
+{
+
+    protected function bootPackage(): void
+    {
+        //
+    }
+    protected function registerPackage(): void
+    {
+        //
+    }
+
+}
