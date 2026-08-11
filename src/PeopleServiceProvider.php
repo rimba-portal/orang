5e4a175404f97e80f60ef793be026f50e3ba1171
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-11 16:16:43 +0800

    Release v1.2.11

--- src/PeopleServiceProvider.php
+++ src/PeopleServiceProvider.php
@@ -6,16 +6,18 @@ namespace Rimba\People;
 
 use Rimba\Base\Services\BitesServiceProvider;
 
+
 class PeopleServiceProvider extends BitesServiceProvider
 {
+
     protected function bootPackage(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         //
     }
-
     protected function registerPackage(): void
     {
         //
     }
+
 }
