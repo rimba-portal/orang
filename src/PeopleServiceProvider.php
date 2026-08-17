e2747df26ae80bf6107c94a9b391c96a8b421612
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-18 07:34:49 +0800

    Release v1.2.17

--- src/PeopleServiceProvider.php
+++ src/PeopleServiceProvider.php
@@ -8,6 +8,8 @@ use Rimba\Base\Services\BitesServiceProvider;
 
 class PeopleServiceProvider extends BitesServiceProvider
 {
+    protected string $iconsPath = __DIR__.'/../resources/svg';
+
     protected function bootPackage(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
